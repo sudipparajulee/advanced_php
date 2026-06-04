290819ed4e76174f7dd6b482b0d6b060709b17b2
Sudip Parajuli <sudip.parajulee@gmail.com>
2026-06-04 08:22:30 +0545

    Add registration page and update login form with registration link

--- register.php
+++ register.php
@@ -1,12 +1,14 @@
 <?php include 'includes/header.php'; ?>
 <div class="h-screen flex items-center justify-center bg-gray-100">
     <div class="bg-white p-4 rounded-lg shadow-md w-1/3">
-        <h2 class="font-bold text-xl text-center mb-4">LOGIN</h2>
+        <h2 class="font-bold text-xl text-center mb-4">REGISTER</h2>
+        <input type="text" class="w-full border p-2 block mb-4 rounded-lg" placeholder="Enter Full Name">
         <input type="text" class="w-full border p-2 block mb-4 rounded-lg" placeholder="Enter Username">
         <input type="password" class="w-full border p-2 block mb-4 rounded-lg" placeholder="Enter Password">
+        <input type="password" class="w-full border p-2 block mb-4 rounded-lg" placeholder="Confirm Password">
         <div class="text-center">
             <input type="submit" class="bg-blue-600 text-white px-4 py-2 rounded-lg cursor-pointer">
-            <p class="mt-2 text-sm">Don't have an account? <a href="register.php" class="text-blue-600">Register</a></p>
+            <p class="mt-2 text-sm">Already have an account? <a href="login.php" class="text-blue-600">Login</a></p>
         </div>
     </div>
 </div>
