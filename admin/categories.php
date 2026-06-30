dea7b4c760bb766e70060ce5444985bc2d73ca2d
Sudip Parajuli <sudip.parajulee@gmail.com>
2026-06-30 08:26:58 +0545

    create-category page

--- admin/categories.php
+++ admin/categories.php
@@ -2,9 +2,29 @@
     <h2 class="text-2xl font-bold">Categories</h2>
     <hr class="h-1 bg-red-500">
     <div class="my-3 flex justify-end">
-        <a href="" class="bg-red-500 hover:bg-red-600 text-white px-3 py-2 rounded-lg">Add Category</a>
+        <a href="createcategory.php" class="bg-red-500 hover:bg-red-600 text-white px-3 py-2 rounded-lg">Add Category</a>
     </div>
-    <table>
-        
+    <table class="w-full">
+        <tr>
+            <th class="border p-2 bg-blue-600 text-white">Order</th>
+            <th class="border p-2 bg-blue-600 text-white">Category Name</th>
+            <th class="border p-2 bg-blue-600 text-white">Action</th>
+        </tr>
+        <tr class="text-center">
+            <td class="border p-2">1</td>
+            <td class="border p-2">National</td>
+            <td class="border p-2">
+                <a href="" class="bg-blue-500 text-white rounded-lg px-2 py-1">Edit</a>
+                <a href="" class="bg-red-500 text-white rounded-lg px-2 py-1">Delete</a>
+            </td>
+        </tr>
+        <tr class="text-center">
+            <td class="border p-2">2</td>
+            <td class="border p-2">International</td>
+            <td class="border p-2">
+                <a href="" class="bg-blue-500 text-white rounded-lg px-2 py-1">Edit</a>
+                <a href="" class="bg-red-500 text-white rounded-lg px-2 py-1">Delete</a>
+            </td>
+        </tr>
     </table>
 <?php include 'footer.php'; ?>
---
+++ admin/createcategory.php
@@ -0,0 +1,12 @@
+<?php include 'header.php'; ?>
+    <h2 class="text-2xl font-bold">Create Category</h2>
+    <hr class="h-1 bg-red-500">
+    <form action="" class="my-3">
+        <input type="text" class="w-full block border p-2 rounded-lg shadow my-5" placeholder="Enter Priority">
+        <input type="text" class="w-full block border p-2 rounded-lg shadow my-5" placeholder="Enter Category Name">
+        <div class="flex justify-center gap-4">
+            <input type="submit" class="bg-blue-500 px-3 py-2 rounded-lg text-white" value="Create Category">
+            <a href="categories.php" class="bg-red-500 px-5 py-2 rounded-lg text-white">Cancel</a>
+        </div>
+    </form>
+<?php include 'footer.php'; ?>
